e7782a529c4856fb1301560bc56a6c8e9e3009f3
a.bouhuolia <a.bouhuolia@live.com>
2022-05-06 18:24:19 +0200

    chore(SecurityForm): add password type to form fields.

--- packages/renderer/src/pages/Account/Security/AccountSecurityForm.tsx
+++ packages/renderer/src/pages/Account/Security/AccountSecurityForm.tsx
@@ -38,6 +38,7 @@ export function AccountSecurityForm() {
         <Stack spacing="lg">
           <TextInput
             id={"password"}
+            type={"password"}
             required
             label={
               <FormattedMessage
@@ -50,6 +51,7 @@ export function AccountSecurityForm() {
           <TextInput
             id={"confirmPassword"}
             required
+            type={"password"}
             label={
               <FormattedMessage
                 id={LangKeys.AccountSecurityFieldRepeatPassword}
@@ -60,6 +62,7 @@ export function AccountSecurityForm() {
           />
           <TextInput
             id={"currentPassword"}
+            type={"password"}
             required
             label={
               <FormattedMessage
