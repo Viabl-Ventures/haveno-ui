31616816be27ee802da7614b409f2e74702f675f
a.bouhuolia <a.bouhuolia@live.com>
2022-05-06 18:38:15 +0200

    chore(AccountSecurity): more stricted password rules.

--- packages/renderer/src/pages/Account/Security/_constants.ts
+++ packages/renderer/src/pages/Account/Security/_constants.ts
@@ -17,19 +17,17 @@
 import * as Yup from "yup";
 
 export const WIDTH = 475;
-const MIN_PASSWORD_CHARS = 8;
 
 export const AccountSecurityFormSchema = Yup.object().shape({
   password: Yup.string()
-    .required("No password provided.")
-    .min(
-      MIN_PASSWORD_CHARS,
-      `Password is too short - should be ${MIN_PASSWORD_CHARS} chars minimum.`
-    )
-    .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+    .required("Please enter your password")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/,
+      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number"
+    ),
   confirmPassword: Yup.string().oneOf(
     [Yup.ref("password"), null],
-    "Passwords must match"
+    "Passwords must match."
   ),
   currentPassword: Yup.string().required("Current password is required"),
 });
