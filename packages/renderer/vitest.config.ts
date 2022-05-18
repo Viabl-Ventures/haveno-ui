9b4a3eed63fbd0ddcaa8f65bcd5b7e22ceb8a1d5
Subir <subir@viabl.ventures>
2022-05-19 03:37:11 +0530

    test: node connect switch atom

--- packages/renderer/vitest.config.ts
+++ packages/renderer/vitest.config.ts
@@ -14,19 +14,24 @@
 //  limitations under the License.
 // =============================================================================
 
+import { mergeConfig } from "vite";
+import viteConfig from "./vite.config";
+
 /**
  * Config for global end-to-end tests
  * placed in project root tests folder
  * @type {import('vite').UserConfig}
  * @see https://vitest.dev/config/
  */
-const config = {
+const config = mergeConfig(viteConfig, {
   test: {
+    setupFiles: ["../../tests/setup-tests.ts"],
+    environment: "happy-dom",
     include: ["./src/**/*.{test,spec}.{ts,tsx}"],
     coverage: {
       reporter: ["html"],
     },
   },
-};
+});
 
 export default config;
