0b79075cdd6c1ed66a623fe4803716e5f6c6b378
a.bouhuolia <a.bouhuolia@live.com>
2022-05-19 19:06:20 +0200

    feat(Modal): styling Modal component.

---
+++ packages/renderer/src/components/atoms/Modal/Modal.tsx
@@ -0,0 +1,27 @@
+import type { ModalProps as MModalProps } from "@mantine/core";
+import { createStyles, Modal as MModal } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  root: {},
+  modal: {
+    borderRadius: theme.radius.xl / 1.6,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 600,
+  },
+}));
+
+export function Modal({ ...props }: MModalProps) {
+  const style = useStyles();
+
+  return (
+    <MModal
+      classNames={style.classes}
+      overlayOpacity={0.25}
+      padding={25}
+      {...props}
+      target="#root"
+    />
+  );
+}
