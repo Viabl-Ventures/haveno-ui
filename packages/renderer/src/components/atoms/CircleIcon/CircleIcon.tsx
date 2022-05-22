06e8e57fdea3f7a2f0d36d59b44bcf68a891dcd4
a.bouhuolia <a.bouhuolia@live.com>
2022-05-22 15:02:13 +0200

    chore(Wallet): Add MoneroBalance and CircleIcon snapshot test.

--- packages/renderer/src/components/atoms/CircleIcon/CircleIcon.tsx
+++ packages/renderer/src/components/atoms/CircleIcon/CircleIcon.tsx
@@ -1,21 +1,49 @@
-import { Box } from "@mantine/core";
+import type { DefaultProps } from "@mantine/core";
+import { Box, createStyles } from "@mantine/core";
 
-export interface CircleIconProps {
+export interface CircleIconProps extends DefaultProps {
+  color?: string;
+  size?: string;
   children: React.ReactNode;
 }
 
-export function CircleIcon({ children }: CircleIconProps) {
+type CircleIconStyle = Pick<CircleIconProps, "color" | "size">;
+
+const useStyles = createStyles((theme, { color, size }: CircleIconStyle) => ({
+  root: {
+    borderRadius: "50%",
+    height: size || 34,
+    width: size || 34,
+    lineHeight: "34px",
+    backgroundColor: "rgba(0, 0, 0, 0.08)",
+    textAlign: "center",
+    color: color || theme.colors.gray[9],
+
+    svg: {
+      position: "relative",
+      top: 2,
+    },
+  },
+}));
+
+export function CircleIcon({
+  children,
+  classNames,
+  className,
+  color,
+  size,
+  ...otherProps
+}: CircleIconProps) {
+  const { classes, cx } = useStyles(
+    { color, size },
+    {
+      name: "CircleIcon",
+      classNames,
+    }
+  );
+
   return (
-    <Box
-      sx={{
-        borderRadius: "50%",
-        height: 34,
-        width: 34,
-        lineHeight: "34px",
-        backgroundColor: "rgba(0, 0, 0, 0.08)",
-        textAlign: "center",
-      }}
-    >
+    <Box className={cx(classes.root, className)} {...otherProps}>
       {children}
     </Box>
   );
