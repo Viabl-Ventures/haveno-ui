69c749b9c9c7c3a4ab8ebcd6fe6900d0aa0a0ba3
Subir <subir@viabl.ventures>
2022-06-10 03:30:23 +0530

    feat: navbar redesign

--- packages/renderer/src/components/templates/NavbarLayout/index.tsx
+++ packages/renderer/src/components/templates/NavbarLayout/index.tsx
@@ -16,8 +16,8 @@
 
 import type { FC } from "react";
 import type { DefaultProps } from "@mantine/core";
-import { Box, createStyles, Group } from "@mantine/core";
-import { Sidebar } from "@organisms/Sidebar";
+import { Box, createStyles, Stack } from "@mantine/core";
+import { Navbar } from "@organisms/Navbar";
 
 export const NavbarLayout: FC<DefaultProps> = ({
   children,
@@ -31,10 +31,10 @@ export const NavbarLayout: FC<DefaultProps> = ({
   });
 
   return (
-    <Group className={cx(classes.container, className)} spacing={0} {...rest}>
-      <Sidebar />
+    <Stack className={cx(classes.container, className)} spacing={0}>
+      <Navbar />
       <Box className={classes.contentArea}>{children}</Box>
-    </Group>
+    </Stack>
   );
 };
 
